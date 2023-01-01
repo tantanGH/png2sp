04af8ce74e48aa7b1ba36d0d86f59fe953bd6058
tantanGH <121137457+tantanGH@users.noreply.github.com>
2023-01-02 06:46:34 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="svg2sp",
+    version="0.0.1",
+    author="tantanGH",
+    author_email="tantanGH@github",
+    license='MIT',
+    description="SVG/PNG format data to X68k sprite data converter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tantanGH/svg2sp",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'svg2sp=svg2sp.svg2sp:main'
+        ]
+    },
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    setup_requires=["setuptools"],
+    install_requires=["Pillow"],
+)
