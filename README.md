519e0442262b4cc7be764dbc5f203a70560f76f5
tantanGH <tantangh68@gmail.com>
2023-01-02 12:41:13 +0900

    release candidate

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # png2sp
 Transparent PNG data to X68k sprite data converter
+
+### Install
+
+    pip install git+https://github.com/tantanGH/png2sp.git
+
+### Usage
+
+    png2sp [options] <input-png-file>
+
+Input movie file can be .mp4 or .avi either.
+
+    options:
+        -o [output-file] ... 出力先テキストファイル名 指定しない場合は標準出力
+        -f [format]      ... r: unsigned short 配列として出力  b: 1ドットごとの unsigned char 配列として出力
+        -x [width]       ... 横サイズ 16の倍数であること
+        -y [height]      ... 縦サイズ 16の倍数であること
+
+Note that you need to add mov2gif installed folder to your PATH environment variable.
+
+
+### Windowsユーザ向けPython導入ガイド
+
+[詳細な日本語での導入ガイド](https://github.com/tantanGH/distribution/blob/main/windows_python_for_x68k.md)
--- png2sp/png2sp.py
+++ png2sp/png2sp.py
@@ -2,7 +2,7 @@ import argparse
 
 from PIL import Image
 
-def convert_png_to_sp(png_file,out_format,out_file,size=(16,16)):
+def convert_png_to_sp(png_file,out_file,out_format='r',size=(16,16)):
 
     # resize
     raw_image = Image.open(png_file)
@@ -32,33 +32,65 @@ def convert_png_to_sp(png_file,out_format,out_file,size=(16,16)):
         else:
             pattern_array.append(b+1)
 
-    if out_format == "b":
+#    for y in range(size[1]):
+#        s = [str(n) for n in pattern_array[ y * size[0] : y * size[0] + size[0] ]]
+#        print(",".join(s))
 
-        # output pattern array data
-        print("/* sprite data */")
-        for y in range(16):
-            s = [str(n) for n in pattern_array[ y * 16 : (y+1) * 16 ]]
-            print(",".join(s)+",")
-    
-    else:
+    # how many 16x16 patterns do we need?
+    patterns_x = ( size[0] - 1 ) // 16 + 1
+    patterns_y = ( size[1] - 1 ) // 16 + 1
 
-        # convert dot data to raw pattern data
-        pattern_data = [ 0 ] * 128
-        for y in range(8):
-            for x in range(4):
-                pattern_data[ 0x00 + y*4 + x ] = ( pattern_array[ (y+0)*16 + (x+0)*2 ] << 4 ) | ( pattern_array[ (y+0)*16 + (x+0)*2 + 1])
-                pattern_data[ 0x20 + y*4 + x ] = ( pattern_array[ (y+8)*16 + (x+0)*2 ] << 4 ) | ( pattern_array[ (y+8)*16 + (x+0)*2 + 1])
-                pattern_data[ 0x40 + y*4 + x ] = ( pattern_array[ (y+0)*16 + (x+4)*2 ] << 4 ) | ( pattern_array[ (y+0)*16 + (x+4)*2 + 1])
-                pattern_data[ 0x60 + y*4 + x ] = ( pattern_array[ (y+8)*16 + (x+4)*2 ] << 4 ) | ( pattern_array[ (y+8)*16 + (x+4)*2 + 1])
+    # output lines
+    output_lines = []
 
-        # output raw pattern data
-        print("/* sprite data */")
-        for y in range(8):
-            tuples = [ tuple( pattern_data[ y * 16 : (y+1) * 16 ]) for i in range(0, 16, 2)]
-            s = [ '0x' + format(t[0],'02x') + format(t[1],'02x') for t in tuples ]
-            print(",".join(s)+",")
+    if out_format != "b":
+        output_lines.append("")
+        output_lines.append(f"unsigned short sp_pattern_data[] = {{")
 
+    # generate patterns by 16x16
+    for py in range(patterns_y):
 
+        for px in range(patterns_x):
+
+            pattern_id = py * patterns_x + px
+
+            width = size[0]
+            ofs = py * width * 16 + px * 16
+
+            if out_format == "b":
+
+                output_lines.append("")
+                output_lines.append(f"/* sprite pattern data {pattern_id} */")
+                output_lines.append(f"unsigned char sp_pattern_data_{pattern_id}[] = {{")
+
+                # output pattern array data
+                for y in range(16):
+                    s = [str(n) for n in pattern_array[ ofs + y * width : ofs + y * width + 16 ]]
+                    output_lines.append("    " + ",".join(s) + ",")
+
+                output_lines.append("};")
+
+            else:
+
+                # convert dot data to raw pattern data
+                pattern_data = [ 0 ] * 128
+                for y in range(8):
+                    for x in range(4):
+                        pattern_data[ 0x00 + y*4 + x ] = ( pattern_array[ ofs + (y+0) * width + (x+0)*2 ] << 4 ) | ( pattern_array[ ofs + (y+0) * width + (x+0)*2 + 1])
+                        pattern_data[ 0x20 + y*4 + x ] = ( pattern_array[ ofs + (y+8) * width + (x+0)*2 ] << 4 ) | ( pattern_array[ ofs + (y+8) * width + (x+0)*2 + 1])
+                        pattern_data[ 0x40 + y*4 + x ] = ( pattern_array[ ofs + (y+0) * width + (x+4)*2 ] << 4 ) | ( pattern_array[ ofs + (y+0) * width + (x+4)*2 + 1])
+                        pattern_data[ 0x60 + y*4 + x ] = ( pattern_array[ ofs + (y+8) * width + (x+4)*2 ] << 4 ) | ( pattern_array[ ofs + (y+8) * width + (x+4)*2 + 1])
+
+                # output raw pattern data
+                output_lines.append("")
+                output_lines.append(f"    /* sprite pattern data {pattern_id} */")
+                for y in range(8):
+                    tuples = [ tuple( pattern_data[ y * 16 + x * 2 : y * 16 + x * 2 + 2 ]) for x in range(8)]
+                    s = [ '0x' + format(t[0],'02x') + format(t[1],'02x') for t in tuples ]
+                    output_lines.append("    " + ",".join(s) + ",")
+
+    if out_format != "b":
+        output_lines.append("};")
 
     # generate palette data
     palette_data = []
@@ -71,25 +103,36 @@ def convert_png_to_sp(png_file,out_format,out_file,size=(16,16)):
         palette_data.append(rgb555)
 
     # output palette data
-    print()
-    print("/* palette data */")
+    output_lines.append("")
+    output_lines.append("/* palette data */")
+    output_lines.append("unsigned short sp_palette_data[] = {")
     s = [ '0x' + format(p,'04x') for p in palette_data ]
-    print(",".join(s)+",")
-    print()
+    output_lines.append("    " + ",".join(s) + ",")
+    output_lines.append("};")
+    output_lines.append("")
+
+    # output to file or stdout
+    if out_file == None:
+        for l in output_lines:
+            print(l)
+    else:
+        with open(out_file, "w") as f:
+            for l in output_lines:
+                f.write(l+"\n")
 
 def main():
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument("infile",help="input PNG file")
-    parser.add_argument("-f","--format",help="output format (b:X-BASIC, r:raw)",default="b")
     parser.add_argument("-o","--outfile",help="output file")
+    parser.add_argument("-f","--format",help="output format (b:X-BASIC, r:raw)",default="b")
     parser.add_argument("-x","--width",help="sprite width (default:16)",type=int,default=16)
     parser.add_argument("-y","--height",help="sprite height (default:16)",type=int,default=16)
 
     args = parser.parse_args()
 
-    convert_png_to_sp(args.infile,args.format,args.outfile,(args.width,args.height))
+    convert_png_to_sp(args.infile,args.outfile,args.format,(args.width,args.height))
 
 
 if __name__ == "__main__":
